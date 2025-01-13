96d8a2b7ff7950f5d715803a27bb72934c32df1c
Shalika <shalika0801@gmail.com>
2025-01-13 17:25:57 +0530

    TopContent

--- lib/main.dart
+++ lib/main.dart
@@ -19,18 +19,51 @@ class _MyAppState extends State<MyApp> {
         
       backgroundColor: Color(0xFF3967D7),
       
-      body: Padding(padding: EdgeInsets.all(10.0),
-      child: 
-      Text('Welcome to Bc Exchange',
+      body: Padding(padding: EdgeInsets.symmetric(vertical: 50,horizontal: 5.0),
       
-      style: TextStyle(
-        fontFamily: 'Cambria',fontSize: 15.0,color: Colors.white,
+     child: Align(
+      alignment: Alignment.topCenter,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+         child: Column(
+          children: [
+           Text('Welcome to Bc Exchange',
+           style: TextStyle(
+                    fontFamily: 'Cambria',
+                    fontSize: 20.0,
+                    color: Colors.white,
+          ),
+
+          
+
         ),
-        )
+        SizedBox(height: 10.0,),
+        Text('We are  offering digital asset exchange with maximum security and advance tracking features',
+           style: TextStyle(
+                    fontFamily: 'Cambria',
+                    fontSize: 15.0,
+                    color: Colors.white,
+          ),
+           textAlign: TextAlign.center, // Centers the text
+                    softWrap: true,
+
+          
+
+        ),
+        
+          ],
+
+         )
+        
+        
+        
+     ),
+    
       )
       
       
       ),
+    ),
     );
   }
 }
